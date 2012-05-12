f41e1140f2e8c67a3728b7fc499b01b4130855d2
Vladimir Sinkov <vladimir.sinkov@gmail.com>
2012-05-13 01:19:02 +0300

    SQL tables

--- SLDauct.sql
+++ SLDauct.sql
@@ -20,3 +20,23 @@ CREATE TABLE `users` (
   PRIMARY KEY (`ID`)
 ) DEFAULT CHARSET=utf8;
 
+CREATE TABLE 'categories' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'name' text NOT NULL,
+  'description' text NOT NULL,
+  PRIMARY KEY('ID')
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE 'items' (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL REFERENCES categories('id'),
+  `price` double NOT NULL,
+  `income_date` date NOT NULL,
+  `period` int(11) NOT NULL,
+  `description` longtext,
+  `manufacturer` text NOT NULL,
+  `name` text NOT NULL,
+  `condition` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
